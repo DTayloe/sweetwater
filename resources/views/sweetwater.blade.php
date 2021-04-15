65fa9adbea749c7bf2bca4f550d0e140f76e81d0
DTayloe <diomoidapple@gmail.com>
2021-04-15 13:27:14 -0400

    first commit

---
+++ resources/views/sweetwater.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Comment Test</title>
+</head>
+<body>
+    <h2>Comments about candy</h2>
+    
+
+    <h2>Comments about call me / don't call me</h2>
+
+
+    <h2>Comments about who referred me</h2>
+
+
+    <h2>Comments about signature requirements upon delivery</h2>
+
+
+    <h2>Miscellaneous comments (everything else)</h2>
+
+
+</body>
+</html>
